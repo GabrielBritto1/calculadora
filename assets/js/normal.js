cc5de8a6e6c8a02f910ffedd1e7972b7109e61d9
Gabriel <gabriel_bianchibritto@hotmail.com>
2023-04-01 17:46:50 -0300

    finalização da calc cientifica

--- assets/js/normal.js
+++ assets/js/normal.js
@@ -2,7 +2,7 @@ let runningTotal = 0;
 let buffer = "0";
 let previousOperator;
 
-const screen = document.querySelector('.screen');
+var screen = document.querySelector('.screen');
 
 function buttonClick(value) {
     if (isNaN(value)) {
